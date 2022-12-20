18989b4ad10d94bbab361ac9b621790aa0ae479a
Iván Rodrigo <irodrigob@users.noreply.github.com>
2022-12-20 22:42:57 +0100

    ts

--- src/systems/infraestructure/frontend/components/dialogAddSystem/dialogAddSystemContainer.tsx
+++ src/systems/infraestructure/frontend/components/dialogAddSystem/dialogAddSystemContainer.tsx
@@ -7,6 +7,7 @@ import {
   FormItem,
   CheckBox,
 } from "@ui5/webcomponents-react";
+import TextField from "@mui/material/TextField";
 import { useForm, Controller } from "react-hook-form";
 import { useTranslations } from "translations/i18nContext";
 import FooterDialog from "systems/infraestructure/frontend/components/dialogAddSystem/footerDialog";
@@ -48,7 +49,71 @@ const DialogAddSystem: FC<Props> = (props) => {
       }
     >
       <Form>
-        <FormItem></FormItem>
+        <FormItem
+          children={
+            <Controller
+              name="name"
+              control={control}
+              defaultValue=""
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  required
+                  label={getI18nText("systems.labelName")}
+                  variant="filled"
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+                  sx={{
+                    width: "15em",
+                    fontFamily: "var(--sapFontFamily)",
+                    fontSize: "var(--sapFontSize)",
+                  }}
+                />
+              )}
+              rules={{ required: getI18nText("general.fieldMandatory") }}
+            />
+          }
+        />
+        <FormItem
+          children={
+            <Controller
+              name="host"
+              control={control}
+              defaultValue=""
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  required
+                  label={getI18nText("systems.labelHOST")}
+                  variant="filled"
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  helperText={
+                    !!error
+                      ? error.type === "validate"
+                        ? getI18nText("editSystem.msgErrorHostInvalid")
+                        : error.message
+                      : null
+                  }
+                  sx={{ width: "30em" }}
+                />
+              )}
+              rules={{
+                required: getI18nText("general.fieldMandatory"),
+                validate: (value) => {
+                  return true; //validateHost(value);
+                },
+              }}
+            />
+          }
+        />
       </Form>
     </Dialog>
   );
